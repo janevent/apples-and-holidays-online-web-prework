db193d6ee5042f3df252d30bf610108bf569e77e
Jane Venter <janeanneventer@gmail.com>
2018-10-21 03:46:48 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,9 @@ def all_supplies_in_holidays(holiday_hash)
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.each do |season, value|
+    value.each do |holiday, supplies|
+      
 end
 
 
